064b0c11b96344807bf3f525e6aa6500b0ac49f1
Kidae-Kim <kidae.kim@daydreamlab.co.kr>
2025-03-04 13:23:18 +0900

    배포

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React, { useEffect } from "react";
-import "aos/dist/aos.css";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NoticePage from "./pages/notice/NoticePage";
-import RootPage from "./pages/root/RootPage";
-import QnAPage from "./pages/qna/QnAPage";
-import AnnouncementPage from "./pages/announcement/AnnouncementPage";
+import React, { useEffect } from 'react';
+import 'aos/dist/aos.css';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NoticePage from './pages/notice/NoticePage';
+import RootPage from './pages/root/RootPage';
+import QnAPage from './pages/qna/QnAPage';
+import AnnouncementPage from './pages/announcement/AnnouncementPage';
 
 function App() {
-  // useEffect(() => {
-  //   if (
-  //     window.location.href === "http://oddbody.art" ||
-  //     window.location.href === "http://oddbody.art/"
-  //   )
-  //     window.location.href = "https://www.oddbody.art";
-  // }, []);
-
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/page">
       <Routes>
         <Route path="/" element={<RootPage />} />
         <Route path="/notice" element={<NoticePage />} />
