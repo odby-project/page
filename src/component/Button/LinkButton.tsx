1e4db8573fe9bf817cc7caac6a2c7b37edd98fd7
Kidae-Kim <kidae.kim@daydreamlab.co.kr>
2025-03-04 14:56:00 +0900

    텍스트 수정

--- src/component/Button/LinkButton.tsx
+++ src/component/Button/LinkButton.tsx
@@ -1,61 +1,47 @@
 import { Button, Stack } from '@mui/material';
 import { ButtonProps } from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
-import i18n from "i18next";
-import '../../i18n/index.js'
-
-
+import i18n from 'i18next';
+import '../../i18n/index.js';
 
 const TranslateButton: React.FC = () => {
-  const openSeaIcon = `${process.env.PUBLIC_URL}/icons/opensea.png`
-  const twitterIcon = `${process.env.PUBLIC_URL}/icons/twitter.png`
-  const instaIcon = `${process.env.PUBLIC_URL}/icons/insta.png`
-  const spatialIcon = `${process.env.PUBLIC_URL}/icons/spatial.png`
+  const openSeaIcon = `${process.env.PUBLIC_URL}/icons/opensea.png`;
+  const twitterIcon = `${process.env.PUBLIC_URL}/icons/twitter.png`;
+  const instaIcon = `${process.env.PUBLIC_URL}/icons/insta.png`;
+  const spatialIcon = `${process.env.PUBLIC_URL}/icons/spatial.png`;
 
   function handleOpensea() {
-    var openNewWindow = window.open("about:blank");
-    if(openNewWindow)
-      openNewWindow.location.href = "https://opensea.io/collection/odby";
+    var openNewWindow = window.open('about:blank');
+    if (openNewWindow)
+      openNewWindow.location.href = 'https://opensea.io/collection/odby';
   }
-  
 
   return (
     <Stack direction="row">
-      <Button
-        onClick={handleOpensea}
-      >
-        <img
-          src={openSeaIcon}
-          alt=""
-          width= "30px"
-        />
+      <Button onClick={handleOpensea}>
+        <img src={openSeaIcon} alt="" width="30px" />
       </Button>
       <Button
         onClick={() => window.open('https://www.twitter.com/oddbody_art')}
       >
-        <img
-          src={twitterIcon}
-          alt=""
-          width="30px"
-        />
+        <img src={twitterIcon} alt="" width="30px" />
       </Button>
       <Button
-        onClick={() => window.open('https://www.instagram.com/oddbody.art', '_blank')}
+        onClick={() =>
+          window.open('https://www.instagram.com/_odby/', '_blank')
+        }
       >
-        <img
-          src={instaIcon}
-          alt=""
-          width="30px"
-        />
+        <img src={instaIcon} alt="" width="30px" />
       </Button>
       <Button
-        onClick={() => window.open('https://www.spatial.io/s/ODBYs-Hi-Fi-Space-6448b78b4ed63407fd6fe82c?share=5487205696407549449', '_blank')}
+        onClick={() =>
+          window.open(
+            'https://www.spatial.io/s/ODBYs-Hi-Fi-Space-6448b78b4ed63407fd6fe82c?share=5487205696407549449',
+            '_blank'
+          )
+        }
       >
-        <img
-          src={spatialIcon}
-          alt=""
-          width="30px"
-        />
+        <img src={spatialIcon} alt="" width="30px" />
       </Button>
     </Stack>
   );
--- src/section/IntroSection.tsx
+++ src/section/IntroSection.tsx
@@ -1,51 +1,53 @@
 import React from 'react';
 import { Box, Stack } from '@mui/material';
 import { isMobile } from 'react-device-detect';
-import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 
 const IntroSection: React.FC = () => {
-  const { t }  = useTranslation(['page'])
-  const image = `${process.env.PUBLIC_URL}/images/OddPerson.png`
+  const { t } = useTranslation(['page']);
+  const image = `${process.env.PUBLIC_URL}/images/OddPerson.png`;
 
   return (
-    <div id="1" className="App-section" >
+    <div id="1" className="App-section">
       <Stack
-        direction={isMobile? "column" : "row"}
-        spacing={isMobile? 1 : -5}
-        sx={{paddingX: isMobile? 5 : 10, paddingBottom: isMobile? 5 : 10, paddingTop: isMobile? 5 : 10}}
+        direction={isMobile ? 'column' : 'row'}
+        spacing={isMobile ? 1 : -5}
+        sx={{
+          paddingX: isMobile ? 5 : 10,
+          paddingBottom: isMobile ? 5 : 10,
+          paddingTop: isMobile ? 5 : 10,
+        }}
       >
-        <Box
-          data-aos={isMobile ? "fade-up" : "fade-right"}
-          data-aos-delay="50" 
-        >
+        <Box data-aos={isMobile ? 'fade-up' : 'fade-right'} data-aos-delay="50">
           <img
-            src={image} 
-            className="App-logo" 
+            src={image}
+            className="App-logo"
             alt="logo"
-            style={{ height: isMobile? "none" : "100%" }}
+            style={{ height: isMobile ? 'none' : '100%' }}
           />
         </Box>
-        <Stack 
-          data-aos={isMobile ? "fade-up" : "fade-left"}
-          data-aos-delay="50" 
-          alignItems="center"   
+        <Stack
+          data-aos={isMobile ? 'fade-up' : 'fade-left'}
+          data-aos-delay="50"
+          alignItems="center"
           justifyContent="center"
           textAlign="left"
-          maxWidth={"600px"}
+          maxWidth={'600px'}
         >
-            <h4>
-              {t('page:intro.header')}
-            </h4>
-            <span className="ODBYODD-BODY-OD">
-              {t('page:intro.content1')} <br />
-              {t('page:intro.content2')} <br />
-              {t('page:intro.content3')} <br />
-              {t('page:intro.content4')} <br />
-              {t('page:intro.content5')} <br />
-              {t('page:intro.content6')} <br />
-              {t('page:intro.content7')} <br />
-              {t('page:intro.content8')}
-            </span>
+          <h4>{t('page:intro.header')}</h4>
+          <span className="ODBYODD-BODY-OD">
+            {t('page:intro.content1')} <br />
+            {t('page:intro.content2')} <br />
+            {t('page:intro.content3')} <br />
+            {t('page:intro.content4')} <br />
+            {t('page:intro.content5')} <br />
+            {t('page:intro.content6')} <br />
+            {t('page:intro.content7')} <br />
+            {t('page:intro.content8')} <br />
+            {t('page:intro.content9')} <br />
+            {t('page:intro.content10')} <br />
+            {t('page:intro.content11')}
+          </span>
         </Stack>
       </Stack>
     </div>
